620e53cae9dc9bb78bc5a80154707b6abc16d4f5
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-01 17:44:47 -0800

    C# DataType project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+    {
+        Console.WriteLine("The Tech Academy");
+        Console.ReadLine();
+        Console.WriteLine("Student Daily Report");
+        Console.ReadLine();
+        Console.WriteLine("What is your name?");
+        String yourname = Console.ReadLine();
+        Console.WriteLine("What course are you on?");
+        String coursename = Console.ReadLine();
+        Console.WriteLine("What page number?");
+        int page = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Do you need help with anything ? Please answer “true” or “false.”");
+        bool myBool = Convert.ToString(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(myBool));// Convert bool to string
+        Console.WriteLine("Were there any positive experiences you’d like to share ? Please give specifics.");
+        String experience = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        String feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.ReadLine();
+
+
+    }
+}
