d55d509eacd234e367dce1f2db99c0009b5571e4
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-02 17:35:57 -0800

    Bool_project

---
+++ Bool_project/Bool_project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bool_project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ever had a DUI, True or False");
+            bool myBool = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            if (age < 15 || ticket >= 3 || myBool != true);
+            {
+                Console.WriteLine("unqualified");
+                Console.ReadLine();
+            }
+          
+        }
+    }
+}
