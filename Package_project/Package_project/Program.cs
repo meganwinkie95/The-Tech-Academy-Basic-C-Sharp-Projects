a384e320dc1d7d83fff720c3b5ec9f565e0eb5de
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-03 17:09:58 -0800

    update to the update Package_project

--- Package_project/Package_project/Program.cs
+++ Package_project/Package_project/Program.cs
@@ -11,30 +11,30 @@ namespace Package_project
             if (weight > 50)
             {
                 Console.WriteLine("your Package is too heavy");
+                Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("What is the package width?");
-            }
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the package height?");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the package length?");
-            int length = Convert.ToInt32(Console.ReadLine());
-            int x = width + height + length;
-            int D = length * height * width * weight;
-            int total = D / 100;
-            if (x > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express");
-            }
-            else
-            {
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+                int x = width + height + length;
+                int D = length * height * width * weight;
+                int total = D / 100;
+                if (x > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express");
+                }
+                else
+                {
 
-                Console.WriteLine("Your estimated total for shipping this package is: $" + total + ".00 Thank you");
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + total + ".00 Thank you");
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
-
         }
     }
 }
