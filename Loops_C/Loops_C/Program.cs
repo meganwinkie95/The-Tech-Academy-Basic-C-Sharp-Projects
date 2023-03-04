44e65247b98e10ada69e42f549a983926b71981e
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-03 17:05:39 -0800

    Loop_C#_project

---
+++ Loops_C/Loops_C/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 52:
+                        Console.WriteLine("You guesse 52. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 62:
+                        Console.WriteLine("You guesse 62. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guesse 12. That is correct");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
