0cbd22232ba4691baab4ab2b95220eaacff9e964
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-06 21:02:39 -0800

    Calling_method

---
+++ CALLINGMETHODSASSIGNMENT/CALLINGMETHODSASSIGNMENT/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExtensions
+{
+    // Extension methods must be defined in a static class.
+    public static class StringExtension
+    {
+        // This is the extension method.
+        // The first parameter takes the "this" modifier
+        // and specifies the type for which the method is defined.
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
---
+++ CALLINGMETHODSASSIGNMENT/CALLINGMETHODSASSIGNMENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods_Simple
+{
+    // Import the extension method namespace.
+    using CustomExtensions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Write something");
+            string s = Convert.ToString(Console.ReadLine());
+
+            // Call the method as if it were an
+            // instance method on the type. Note that the first
+            // parameter is not specified by the calling code.
+            int i = s.WordCount();
+            System.Console.WriteLine("Word count of your sentence is {0}", i);
+            Console.ReadLine();
+        }
+    }
+}
