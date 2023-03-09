69c208c413166fc529c6444b5bae412c2330b661
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-09 12:48:02 -0800

    update methods

---
+++ Methods1/Methods1/Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Methods1;
+
+namespace Methods1
+{
+    public class Numbers
+    {
+        public int add(int x)
+        {
+            return x + 2;
+        }
+        public int sub(int x)
+        {
+            return x - 2;
+        }
+        public int mult(int x)
+        {
+            return x * 2;
+        }
+
+    }
+}
---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            // Call the constructor that has no parameters.
+            Console.WriteLine("Pick a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Numbers y = new Numbers();
+            Numbers T = new Numbers();
+            Numbers R = new Numbers();
+            int solution3 = R.mult(number);
+            int solution2 = T.sub(number);
+            int solution1 = y.add(number);
+            Console.WriteLine(solution1);
+            Console.WriteLine(solution2);
+            Console.WriteLine(solution3);
+            Console.ReadLine();
+        }
+         
+    }
+}
