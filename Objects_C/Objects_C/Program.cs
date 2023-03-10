85bb4f63bc64ecaf2605679fc8bc1badfc8245ca
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-10 15:21:58 -0800

    objectsC

---
+++ Objects_C/Objects_C/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Continue4
+{
+    public class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        public void SayName(string x, string y, int k)
+        {
+            Console.WriteLine("Name: " + x + " " + " " + y + " " + k);
+            Console.ReadLine();
+        }
+
+    }
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+
+    public class Program
+    {
+
+        static void Main()
+        {
+            Employee name = new Employee();
+            name.FName = "sample";
+            name.LName = "student";
+            name.ID = 9;
+            name.SayName(name.FName, name.LName, name.ID);
+        }
+    }
+}
