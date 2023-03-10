1f9450b1e3d2b4c7c509113b6d699003d72a7060
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-09 17:15:40 -0800

    Methods 2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void MyMethod(int k,  int x = 2)
+        {
+            Console.WriteLine(k + x);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a number(optional)");
+            string num2 = (Console.ReadLine());
+            if (num2 == "")
+            {
+                MyMethod(num1);
+            }
+            else
+            {
+                int y = Convert.ToInt32(num2);
+                MyMethod(num1, y);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
