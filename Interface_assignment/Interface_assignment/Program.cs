225c05db8b02e4de89427d08eaf02f669e5e8c11
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-10 16:55:33 -0800

    Interface_method

---
+++ Interface_assignment/Interface_assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_assignment
+{
+   interface IQuittable
+    {
+        void Quit();
+    }
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Welcome Employee");
+        }
+    }
+
+    public class Program
+    {
+
+        static void Main()
+        {
+            Employee name = new Employee();
+            name.Quit();
+            Console.ReadLine();
+        }
+        
+    }
+}
+
