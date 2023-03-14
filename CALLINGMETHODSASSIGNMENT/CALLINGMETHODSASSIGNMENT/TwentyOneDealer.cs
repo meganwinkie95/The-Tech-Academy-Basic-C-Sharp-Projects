41c1db622ab750b3371ffd7e257c4e043a684af9
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-14 14:14:05 -0700

    DateTime_Assignment

---
+++ CALLINGMETHODSASSIGNMENT/CALLINGMETHODSASSIGNMENT/TwentyOneDealer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLINGMETHODSASSIGNMENT
+{
+    public class TwentyOneDealer : Dealer
+    {
+        public List<Card> Hand { get; set; }
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+        public Deck deck { get; set; }
+    }
+  
+}
---
+++ DateTime_Assignment/DateTime_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime currTimeAndDate = DateTime.Now;
+            Console.WriteLine("Today's time and date is {0}\n", currTimeAndDate);
+            Console.WriteLine("Enter a number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            DateTime Xhours = currTimeAndDate.AddHours(y);
+            Console.WriteLine(string.Format("{0} {1}", currTimeAndDate, Xhours));
+            Console.ReadLine();
+        }
+
+        }
+}
