34a3af3bf34c9391b7b854c3069b90060f8a47de
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-14 15:04:54 -0700

    Constructor_Assignment

---
+++ Constructor_Assignment/Constructor_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class ConstructMe
+    {
+        public string name;
+        public int id;
+        public ConstructMe(string Name, int id)
+        {
+            this.name = Name;
+            this.id = id;
+        }
+
+    }
+    class cnstrct
+    {
+        static void Main(string[] args)
+        {
+            var a  = new ConstructMe("Daniel", 28);
+            Console.WriteLine($"The instance with name: {nameof(a)}, has property name: {a.name} and id: {a.id}");
+            Console.ReadLine(); ;
+        }
+    }
+}
