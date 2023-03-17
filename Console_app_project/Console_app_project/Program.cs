6dbd4c3033c0d9a9214e03836de00cf1168b048f
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-17 12:30:35 -0700

    Final_Challenge

---
+++ Console_app_project/Console_app_project/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_app_project
+{
+  public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.WriteLine(stud.StudentName);
+                Console.ReadLine();
+
+            }
+        }
+    }
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+       
+    }
+   
+    
+}
